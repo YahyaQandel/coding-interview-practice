be0faba1ab6f46c78e65ec2527419e790bd8cf0e
YahyaQandel <yahya.qandel@gmail.com>
2022-10-21 21:37:40 +0200

    add some tree functionality with tests

--- tree/mtree.py
+++ tree/mtree.py
@@ -1,5 +1,6 @@
 from collections import deque
 from itertools import count
+from statistics import mode
 from node import Node
 class Tree():
     def __init__(self,root) -> None:
@@ -126,3 +127,100 @@ class BSTree:
                 return max(lheight,rheight)
             return 0
         return _height(self.root)
+    
+    def delete_tree(self):
+        self.root = None  # not a good option as we don't have garbage collector to remove other nodes
+                          # without closing the app
+    
+    def get_min(self):
+        '''this version is working on a balanced tree
+        '''
+        if not self.root:
+            return None
+
+        if self.root.left:
+            return self._get_lowest_node_value_of_branch(self.root.left)
+        return self.root.value
+
+    def get_min_unbalanced(self):
+        '''this will loop throough all tree items
+        '''
+        def _min(node):
+            if not node:
+                return 10000000000000
+            left_min = min(_min(node.left),node.value)
+            right_min = min(_min(node.right),node.value)
+            return min(left_min,right_min)
+        return _min(self.root)
+
+    def get_max(self):
+        '''this version is working on a balanced tree
+        '''
+        if not self.root:
+            return None
+
+        def _max(node):
+            if not node.right:
+                return node.value
+            return _max(node.right)
+        
+        return _max(self.root)
+
+
+    def get_max_unbalanced(self):
+        '''this will loop throough all tree items
+        '''
+        def _max(node):
+            if not node:
+                return -10000000000000
+            left_min = max(_max(node.left),node.value)
+            right_min = max(_max(node.right),node.value)
+            return max(left_min,right_min)
+        return _max(self.root)
+    
+
+    def get_successor(self,value):
+        '''successor in binary search tree by traversing the tree
+        in order'''
+
+        def inorder(node,sorted_tree_as_list,value_index):
+            if not node:
+                return None
+            left = inorder(node.left,sorted_tree_as_list,value_index)
+            sorted_tree_as_list.append(node.value)
+            if node.value == value:
+                value_index = len(sorted_tree_as_list)
+            right = inorder(node.right,sorted_tree_as_list,value_index)
+            return value_index or left or right
+        sorted_tree_as_list = []
+        value_index = inorder(self.root,sorted_tree_as_list,0)
+        if not value_index:
+            return -1
+        return sorted_tree_as_list[value_index]
+
+    
+    def get_parent(self,value):
+        '''we can get parent by traversing inorder'''
+        def _parent(node,parent):
+            if not node:
+                return None
+            left_parent = _parent(node.left,node)
+            if node.value == value:
+                return parent
+            right_parent = _parent(node.right,node)
+            return left_parent or right_parent
+        return _parent(self.root,None)
+
+
+    
+    def print_values(self):
+        '''uses inorder traversal to create a sorted list of a tree'''
+        def inorder(node,sorted_tree_as_list):
+            if not node:
+                return None
+            left = inorder(node.left,sorted_tree_as_list,)
+            sorted_tree_as_list.append(node.value)
+            right = inorder(node.right,sorted_tree_as_list)
+        sorted_tree_as_list = []
+        inorder(self.root,sorted_tree_as_list)
+        return sorted_tree_as_list
--- tree/test_bst.py
+++ tree/test_bst.py
@@ -119,3 +119,137 @@ class BinarySearchTest(TestCase):
         self.bstree.insert(4)
         self.bstree.insert(2)
         self.assertEqual(self.bstree.get_height(),4)
+
+
+    def test_delete_tree(self):
+        self.bstree.insert(10)
+        self.bstree.insert(20)
+        self.bstree.delete_tree()
+        self.assertIsNone(self.bstree.root)
+
+    
+    def test_get_min(self):
+        self.bstree.insert(10)
+        self.bstree.insert(20)
+        self.bstree.insert(15)
+        self.bstree.insert(5)
+        self.bstree.insert(-2)
+        self.bstree.insert(3)
+        self.bstree.insert(100)
+        self.bstree.insert(-1)
+        self.assertEqual(self.bstree.get_min(),-2)
+
+    def test_get_min_unbalanced(self):
+        self.bstree.insert(10)
+        self.bstree.insert(20)
+        self.bstree.insert(15)
+        self.bstree.insert(5)
+        self.bstree.insert(-2)
+        self.bstree.insert(3)
+        self.bstree.insert(100)
+        self.bstree.insert(-1)
+        self.assertEqual(self.bstree.get_min_unbalanced(),-2)
+        self.bstree.delete(-2)
+        self.assertEqual(self.bstree.get_min_unbalanced(),-1)
+        self.bstree.delete(-1)
+        self.assertEqual(self.bstree.get_min_unbalanced(),3)
+        self.bstree.delete(3)
+        self.assertEqual(self.bstree.get_min_unbalanced(),5)
+        self.bstree.delete(5)
+        self.assertEqual(self.bstree.get_min_unbalanced(),10)
+        self.bstree.delete(10)
+        self.assertEqual(self.bstree.get_min_unbalanced(),15)
+    
+    def test_get_min_max_single_node(self):
+        self.bstree.insert(10)
+        self.assertEqual(self.bstree.get_max(),10)
+        self.assertEqual(self.bstree.get_min(),10)
+
+    def test_get_max(self):
+        self.bstree.insert(10)
+        self.bstree.insert(20)
+        self.bstree.insert(15)
+        self.bstree.insert(5)
+        self.bstree.insert(-2)
+        self.bstree.insert(3)
+        self.bstree.insert(100)
+        self.bstree.insert(-1)
+        self.assertEqual(self.bstree.get_max(),100)
+        self.bstree.delete(100)
+        self.assertEqual(self.bstree.get_max(),20)
+    
+    def test_get_max_unbalanced(self):
+        self.bstree.insert(10)
+        self.bstree.insert(20)
+        self.bstree.insert(15)
+        self.bstree.insert(5)
+        self.bstree.insert(-2)
+        self.bstree.insert(3)
+        self.bstree.insert(100)
+        self.bstree.insert(-1)
+        self.assertEqual(self.bstree.get_max_unbalanced(),100)
+        self.bstree.delete(100)
+        self.assertEqual(self.bstree.get_max_unbalanced(),20)
+        self.bstree.delete(20)
+        self.assertEqual(self.bstree.get_max_unbalanced(),15)
+        self.bstree.delete(15)
+        self.assertEqual(self.bstree.get_max_unbalanced(),10)
+        self.bstree.delete(10)
+        self.assertEqual(self.bstree.get_max_unbalanced(),5)
+        self.bstree.delete(5)
+        self.assertEqual(self.bstree.get_max_unbalanced(),3)
+
+
+    
+    def test_get_successor(self):
+        self.bstree.insert(10)
+        self.bstree.insert(5)
+        self.bstree.insert(15)
+        self.bstree.insert(3)
+        self.bstree.insert(7)
+        self.bstree.insert(13)
+        self.bstree.insert(20)
+        self.assertEqual(self.bstree.get_successor(3),5)
+        self.assertEqual(self.bstree.get_successor(5),7)
+        self.assertEqual(self.bstree.get_successor(7),10)
+        self.assertEqual(self.bstree.get_successor(10),13)
+        self.assertEqual(self.bstree.get_successor(13),15)
+        self.assertEqual(self.bstree.get_successor(15),20)
+
+    def test_get_successor_of_non_existing_node(self):
+        self.bstree.insert(10)
+        self.bstree.insert(5)
+        self.bstree.insert(15)
+        self.bstree.insert(3)
+        self.bstree.insert(7)
+        self.bstree.insert(13)
+        self.bstree.insert(20)
+        self.assertEqual(self.bstree.get_successor(11),-1)
+
+
+    
+    def test_get_parent_for_node(self):
+        self.bstree.insert(10)
+        self.bstree.insert(5)
+        self.bstree.insert(15)
+        self.bstree.insert(3)
+        self.bstree.insert(7)
+        self.bstree.insert(13)
+        self.bstree.insert(20)
+        self.assertEqual(self.bstree.get_parent(10),None)
+        self.assertEqual(self.bstree.get_parent(5).value,10)
+        self.assertEqual(self.bstree.get_parent(20).value,15)
+
+
+    
+    def test_print_values(self):
+        self.bstree.insert(10)
+        self.bstree.insert(5)
+        self.bstree.insert(15)
+        self.bstree.insert(3)
+        self.bstree.insert(7)
+        self.bstree.insert(13)
+        self.bstree.insert(20)
+        self.assertEqual(self.bstree.print_values(),[3,5,7,10,13,15,20])
+        self.bstree.insert(-1)
+        self.assertEqual(self.bstree.print_values(),[-1,3,5,7,10,13,15,20])
