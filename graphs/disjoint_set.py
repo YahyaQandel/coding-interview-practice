6c02b08fbe49fc530fb0d2337ccecc15a0c57d79
YahyaQandel <yahya.qandel@gmail.com>
2022-11-20 03:43:34 +0200

    enhance disjoint algorithm adding (path compression and quick union by rank )

--- graphs/disjoint_set.py
+++ graphs/disjoint_set.py
@@ -38,5 +38,19 @@ class DisJoingSetQuickUnion():
         if root_first_el != root_second_el:
             self.root[second_el] = first_el
 
+    def find_root_optimizied_path_compression(self,el):
+        if el == self.root[el]:
+            return el
+        self.root[el] = self.find_root(el)
+        return self.root[el] 
+    
+    def union_optimized_by_rank(self,first_el,second_el):
+        if self.rank[first_el] > self.rank[second_el]:
+            self.root[second_el] = first_el
+        elif self.rank[first_el] < self.rank[second_el]:
+            self.root[first_el] = second_el
+        else:
+            self.root[second_el] = first_el
+            self.rank[first_el] += 1
     def connected(self,first_el,second_el):
         return self.find_root(first_el) == self.find_root(second_el)
