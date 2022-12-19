58ab267db583e856a086153a5eb120d3dac65caf
YahyaQandel <yahya.qandel@gmail.com>
2022-12-20 00:19:50 +0200

    add bubble sort

---
+++ sort/bubblesort.py
@@ -0,0 +1,7 @@
+class BubbleSort():
+    def sort(self,arr):
+        for i in range(len(arr) - 1):
+            for j in range(len(arr) - 1 - i):
+                if arr[j] > arr[j+1]:
+                    arr[j],arr[j+1] = arr[j+1],arr[j]
+        return arr
